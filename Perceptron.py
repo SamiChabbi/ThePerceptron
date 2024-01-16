ea0e09507844336b14618bb466206b3ce9b7ac66
Himilco <sami.chabbi.42@gmail.com>
2024-01-16 08:58:26 +0100

    add dockstrings

--- Perceptron.py
+++ Perceptron.py
@@ -1,66 +1,153 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 13 15:07:58 2022
+# Perceptron.py
 
-@author: himilco
-"""
 import numpy as np
 
 def sigmoid(z):
+    """
+    Apply the sigmoid activation function.
+
+    Parameters:
+    - z: Input value.
+
+    Returns:
+    - Output value after applying the sigmoid function.
+    """
     return 1 / (1 + np.exp(-z))
 
 def linear_function(X, W, b):
-    linear_y_hat = np.dot(X , W) + b
-    return linear_y_hat
+    """
+    Compute the linear function.
+
+    Parameters:
+    - X: Input matrix.
+    - W: Weights matrix.
+    - b: Bias term.
+
+    Returns:
+    - Linear output.
+    """
+    return np.dot(X, W) + b
 
 class Neurone:
-    '''Logistical regression single neuron'''
+    """
+    Logistical regression single neuron.
+    """
     def __init__(self, nb_features):
-        print("Creating new neuron")
-        self.W, self.b = self.init_param(nb_features)
-        
+        """
+        Initialize a neuron with random parameters.
+
+        Parameters:
+        - nb_features: Number of input features.
+        """
+        self.W, self.b = self.init_params(nb_features)
+
     def forward(self, X, Y):
+        """
+        Perform forward propagation.
+
+        Parameters:
+        - X: Input matrix.
+        - Y: Target values.
+
+        Returns:
+        - cost: Computed cost.
+        - Y_hat: Predicted values.
+        """
         W = self.W
         b = self.b
-        Z =linear_function(X, W, b)
+        Z = linear_function(X, W, b)
         Y_hat = sigmoid(Z)
         m = X.shape[1]
-        cost = - 1/m * np.sum(Y * np.log(Y_hat) + (1 - Y) * np.log(1 - Y_hat))
-        return cost , Y_hat
+        cost = -1/m * np.sum(Y * np.log(Y_hat) + (1 - Y) * np.log(1 - Y_hat))
+        return cost, Y_hat
 
     def backward(self, Y, Y_hat, X, learning_rate):
+        """
+        Perform backward propagation and update parameters.
+
+        Parameters:
+        - Y: Target values.
+        - Y_hat: Predicted values.
+        - X: Input matrix.
+        - learning_rate: Learning rate for gradient descent.
+
+        Returns:
+        - Updated weights (W) and bias (b).
+        """
         W = self.W
         b = self.b
         m = X.shape[1]
-        d_W = 1/m *(np.dot(X.T,(Y_hat - Y)))
+        d_W = 1/m * (np.dot(X.T, (Y_hat - Y)))
         d_b = 1/m * np.sum(Y_hat - Y)
         n_W = W - learning_rate * d_W
         n_b = b - learning_rate * d_b
         return n_W, n_b
 
-    def init_param(self, dim):
+    def init_params(self, dim):
+        """
+        Initialize weights (W) and bias (b) parameters.
+
+        Parameters:
+        - dim: Number of input features.
+
+        Returns:
+        - W: Initialized weights.
+        - b: Initialized bias.
+        """
         W = np.zeros((dim, 1))
         b = 0
         return W, b
-    
+
     def compute_performance(self, X, Y_):
+        """
+        Compute the performance of the model.
+
+        Parameters:
+        - X: Input matrix.
+        - Y_: Target values.
+
+        Returns:
+        - Percentage of correct predictions.
+        """
         Y = Y_.T
         W = self.W
         b = self.b
-        Z =linear_function(X, W, b)
+        Z = linear_function(X, W, b)
         Y_hat = sigmoid(Z)
         correct_prediction_tab = (Y_hat > 0.5) == Y
         correct_prediction_count = np.count_nonzero(correct_prediction_tab == True)
-        percentage = (correct_prediction_count * 100)/ correct_prediction_tab.size
-        return percentage  
-    
+        percentage = (correct_prediction_count * 100) / correct_prediction_tab.size
+        return percentage
+
     def predict(self, X):
-        Z =linear_function(X, self.W, self.b)
+        """
+        Make predictions using the trained model.
+
+        Parameters:
+        - X: Input matrix.
+
+        Returns:
+        - Predicted values.
+        """
+        Z = linear_function(X, self.W, self.b)
         Y_hat = sigmoid(Z)
         return Y_hat
-    
+
     def train(self, X, Y_, epochs, lr):
+        """
+        Train the model using gradient descent.
+
+        Parameters:
+        - X: Input matrix.
+        - Y_: Target values.
+        - epochs: Number of training epochs.
+        - lr: Learning rate for gradient descent.
+
+        Returns:
+        - costs: List of costs over training.
+        - W: Trained weights.
+        - b: Trained bias.
+        """
         Y = Y_.T
         costs = []
         for i in range(0, epochs):
@@ -71,8 +158,3 @@ class Neurone:
             costs.append(cost)
             self.W, self.b = self.backward(Y, Y_hat, X, lr)
         return costs, W, b
-    
-
-
-
-
--- main.py
+++ main.py
@@ -1,38 +1,45 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 13 16:24:37 2022
+# main.py
 
-@author: himilco
-"""
-import numpy as np 
-import  matplotlib.pyplot as plt 
-import h5py 
-import Perceptron 
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
 from PIL import Image
+from Perceptron import Neurone
+import random
 
 def load_dataset():
-    with h5py.File('datasets/train_catvnoncat.h5', "r") as train_dataset:
-        train_set_x_orig = np.array(train_dataset["train_set_x"][:])
-        train_set_y_orig = np.array(train_dataset["train_set_y"][:])
+    """
+    Load the cat vs. non-cat dataset.
 
-    with h5py.File('datasets/test_catvnoncat.h5', "r") as test_dataset:
-        test_set_x_orig = np.array(test_dataset["test_set_x"][:])
-        test_set_y_orig = np.array(test_dataset["test_set_y"][:])
-        classes = np.array(test_dataset["list_classes"][:])
+    Returns:
+    - train_set_x_orig: Training dataset features.
+    - train_set_y_orig: Training dataset labels.
+    - test_set_x_orig: Test dataset features.
+    - test_set_y_orig: Test dataset labels.
+    - classes: List of classes.
+    """
+    with h5py.File('datasets/train_catvnoncat.h5', 'r') as train_dataset:
+        train_set_x_orig = np.array(train_dataset['train_set_x'][:])
+        train_set_y_orig = np.array(train_dataset['train_set_y'][:])
+
+    with h5py.File('datasets/test_catvnoncat.h5', 'r') as test_dataset:
+        test_set_x_orig = np.array(test_dataset['test_set_x'][:])
+        test_set_y_orig = np.array(test_dataset['test_set_y'][:])
+        classes = np.array(test_dataset['list_classes'][:])
 
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
 
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes 
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
+# Loading dataset
 x_train, y_train, x_test, y_test, classes = load_dataset()
-print("Size of the training dataset : ",y_train.shape)
-print("Size of the testing dataset : ",y_test.shape)
-print("Size of the image : ",x_train[0].shape)
-print("shape of training dataset : ",x_train.shape)
-img = x_train[11]
-x_train.shape
+print("Size of the training dataset: ", y_train.shape)
+print("Size of the testing dataset: ", y_test.shape)
+print("Size of the image: ", x_train[0].shape)
+print("Shape of training dataset: ", x_train.shape)
+
+# Preprocessing
 flat_x_train = x_train.reshape(x_train.shape[0], -1)
 flat_x_test = x_test.reshape(x_test.shape[0], -1)
 print("flat_x_train.shape", flat_x_train.shape)
@@ -40,17 +47,34 @@ print("flat_x_test.shape", flat_x_test.shape)
 train_set_x = flat_x_train / 255
 test_set_x = flat_x_test / 255
 
+# Training the perceptron
 X = train_set_x
 Y = y_train
-i = 2
+i = random.randint(0, x_test.shape[0] - 1)
 
-n_r = Perceptron.Neurone(X.shape[1])
-costs, W, b = n_r.train(X, Y, 2000, 0.1)
+# Creating a Neurone instance
+n_r = Neurone(X.shape[1])
+
+# Training the model
+costs, W, b = n_r.train(X, Y, epochs=2000, lr=0.1)
+
+# Evaluating performance on the test set
 perf = n_r.compute_performance(test_set_x, y_test)
+
+# Making a prediction on a specific example
 prediction = n_r.predict(train_set_x[i])
+
+# Displaying the example image
 plt.imshow(x_test[i])
 plt.show()
-print("performance : ",perf)
-print("prediction : ",round(float(prediction), 4))
-plt.plot(costs, np.linspace(0,len(costs) - 1,len(costs)))
+
+# Displaying results
+print("Performance: ", perf)
+print("Prediction: ", round(float(prediction[0]), 4))
+
+# Plotting the cost over training epochs
+plt.plot(costs, np.linspace(0, len(costs) - 1, len(costs)))
+plt.xlabel("Epochs")
+plt.ylabel("Cost")
+plt.title("Cost over Training Epochs")
 plt.show()
